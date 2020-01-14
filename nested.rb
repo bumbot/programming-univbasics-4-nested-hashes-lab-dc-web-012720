a329ed31ce86f3c9463636543199c7f1f2b34524
Paul Kim <pkim1295@gmail.com>
2020-01-14 22:36:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,6 +84,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer-hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 
     
 end
